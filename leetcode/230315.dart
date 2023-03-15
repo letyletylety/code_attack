209a2d260fb40c61cccc164df5cb7eab9bdd0c5b
Lety <taej0127@gmail.com>
2023-03-15 12:00:57 +0900

    first commit

---
+++ leetcode/230315.dart
@@ -0,0 +1,17 @@
+class TreeNode {
+  int val;
+  TreeNode? left;
+  TreeNode? right;
+  TreeNode([this.val = 0, this.left, this.right]);
+}
+
+class Solution {
+  bool isCompleteTree(TreeNode? root) {
+
+    isCompleteTree(root)
+
+    return false;
+  }
+
+
+}
